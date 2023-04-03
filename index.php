faa999e74ffecc46e44be8cb29e4c7581c825118
Michael <michael.sullivan123@hotmail.co.uk>
2023-04-03 14:02:25 +0100

    Basic webpage

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Book Collection</title>
+
+    <meta name="description" content="Project 2 collection app displaying my book collection">
+    <meta name="author" content="Michael Sullivan">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="icon" href="images/favicon.png" sizes="192x192">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+
+    <!-- <script defer src="js/index.js"></script> -->
+</head>
+
+<body>
+
+<h1>Book Collection</h1>
+
+<!--Make the below into a loop using the DBO item-->
+
+<div class="collection">
+    <div class="book">
+        <img class="book-image" src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg" alt="Book Cover for //name">
+        <div class="book-head-container">
+            <p class="book-head">Book 1 title</p>
+            <p class="book-head align-right">Year</p>
+            <p class="book-head">Author</p>
+            <p class="book-head align-right">Genre</p>
+        </div>
+        <hr class="book-hr">
+        <div class="book-details-container">
+            <p class="book-detail">Comp%</p>
+            <p class="book-detail">My Rating</p>
+            <p href="link" class="book-detail">Link</p>
+        </div>
+    </div>
+
+    <div class="book">
+        <img class="book-image" src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1631501298i/1898.jpg" alt="Book Cover for //name">
+        <div class="book-head-container">
+            <p class="book-head">Book 2 title</p>
+            <p class="book-head align-right">Year</p>
+            <p class="book-head">Author</p>
+            <p class="book-head align-right">Genre</p>
+        </div>
+        <hr class="book-hr">
+        <div class="book-details-container">
+            <p class="book-detail">Comp%</p>
+            <p class="book-detail">My Rating</p>
+            <p href="link" class="book-detail">Link</p>
+        </div>
+    </div>
+
+
+<?php
+
+?>
+
+</body>
+</html>
